b47915ff07c0c15e51d15b0f505b1ff60760a742
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-12-10 23:23:05 +0000

    feat: make header section responsive

--- src/features-section/FeatureSection.tsx
+++ src/features-section/FeatureSection.tsx
@@ -19,11 +19,11 @@ export const FeatureSection = () => {
       className="bg-[url('https://cdn.auth0.com/website/cic-homepage/bg-mobile/BG_Mobile.svg')]
     bg-top bg-no-repeat bg-cover rounded-[2.4rem] min-h-fit text-white  py-16 max-w-[90rem] mx-auto"
     >
-      <div className="pl-16">
-        <h2 className="text-6xl max-w-[50%] mb-6">
+      <div className="pl-10 lg:pl-16">
+        <h2 className="md:text-4xl lg:text-6xl max-w-[50%] mb-3 lg:mb-6">
           Identity is so much more than just the login box
         </h2>
-        <p className="text-3xl font-light mb-14">
+        <p className="md:text-xl lg:text-3xl font-light mb-14">
           Let’s take a look at everything you can do.
         </p>
       </div>
